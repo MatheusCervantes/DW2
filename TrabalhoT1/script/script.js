8f6b3cbb1b574f9b3deb27d9a067e88f765c691e
MatheusCervantes <matheuscervantes1@gmail.com>
2024-03-29 09:06:08 -0300

    29/03/2024

--- TrabalhoT1/script/script.js
+++ TrabalhoT1/script/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     var pagina_atual = window.location.pathname.split("/").pop();
-    var link = 'calcular_acessos.php?pagina_atual=' + pagina_atual;
     $.ajax({
         type: 'POST',
         url: 'calcular_acessos.php',
