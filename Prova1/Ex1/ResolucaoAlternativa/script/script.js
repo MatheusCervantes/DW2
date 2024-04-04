70786009b18aa32cd1047b12d6a2cefeccfa8d7d
MatheusCervantes <matheuscervantes1@gmail.com>
2024-04-04 14:24:07 -0300

    04/04/2024

--- Prova1/Ex1/ResolucaoAlternativa/script/script.js
+++ Prova1/Ex1/ResolucaoAlternativa/script/script.js
@@ -1,17 +1,3 @@
-/*$('#titulo, #alinhamento_pag, #corpo, #imagem, #mostrarimagem, #cor').blur(function () {
-    var id = $(this).attr('id');
-    var value = $(this).val();
-    $.ajax({
-        type: 'POST',
-        url: 'ajax.php',
-        async: true,
-        data: { id: id, value: value },
-        success: function() {
-            console.log('Cookie do campo #' + id + ' criado.');
-        }
-    });
-});*/
-
 $('#limparcookies').click(function () {
     id = $(this).attr('id');
     value = '';
