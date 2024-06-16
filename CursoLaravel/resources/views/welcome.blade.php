a7b203fb5d660678c2206cc0341b2ac49a5ef896
MatheusCervantes <matheuscervantes1@gmail.com>
2024-06-16 17:34:15 -0300

    Aula 5

--- CursoLaravel/resources/views/welcome.blade.php
+++ CursoLaravel/resources/views/welcome.blade.php
@@ -845,6 +845,27 @@
     @else
     <p>O nome não é Pedro</p>
     @endif
+
+    @for($i=0; $i < count($arr); $i++) <p> {{$arr[$i]}} </p>
+        @if ($i == 2)
+        <p> i é 2</p>
+        @endif
+
+        @endfor
+
+        @foreach($nomes as $nome)
+        <p>{{$loop->index}}</p>
+        <p>{{$nome}}</p>
+        @endforeach
+
+        @php
+        echo "dadada";
+        @endphp
+
+
+
+        <!-- Comentário em HTML -->
+        {{-- Comentário em BLADE --}}
 </body>
 
 </html>
--- CursoLaravel/routes/web.php
+++ CursoLaravel/routes/web.php
@@ -16,8 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $nome = "Matheus";
     $idade = 15;
-    $prof = "Programador";
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "programador"]);
+    $arr = [1, 2, 3, 4, 5, 6];
+    $nomes = ["Marcos", "Gerson", "Rogério"];
+    return view(
+        'welcome',
+        [
+            'nome' => $nome,
+            'idade' => $idade,
+            'profissao' => "programador",
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]
+    );
 });
 
 Route::get('/contact', function () {
