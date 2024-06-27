d84fd8f830c3dfd8009d30e8c53f0fc40e01bd0c
MatheusCervantes <matheuscervantes1@gmail.com>
2024-06-26 22:53:11 -0300

    Aula 21

--- CursoLaravel/app/Http/Controllers/EventController.php
+++ CursoLaravel/app/Http/Controllers/EventController.php
@@ -9,9 +9,16 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::all();
+        $search = request('search');
 
-        return view('welcome', ['events' => $events]);
+        if ($search) {
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%']
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -19,7 +26,8 @@ class EventController extends Controller
         return view('events.create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $event  = new Event;
 
         $event->title = $request->title;
@@ -30,7 +38,7 @@ class EventController extends Controller
         $event->items = $request->items;
 
         // Image Upload
-        if($request->hasFile('image') && $request->file('image')->isValid()) {
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $requestImage = $request->image;
             $extension = $requestImage->extension();
             $imageName = md5($requestImage->getClientOriginalName() . strtotime('now') . "." . $extension);
@@ -43,7 +51,8 @@ class EventController extends Controller
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
-    public function show($id) {
+    public function show($id)
+    {
         $event = Event::findOrFail($id);
         return view('events.show', ['event' => $event]);
     }
