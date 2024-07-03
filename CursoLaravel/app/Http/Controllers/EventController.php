fca01099f7608b2656f7890b9131366b4d0a5beb
MatheusCervantes <matheuscervantes1@gmail.com>
2024-07-03 15:18:46 -0300

    Aula 30

--- CursoLaravel/app/Http/Controllers/EventController.php
+++ CursoLaravel/app/Http/Controllers/EventController.php
@@ -58,9 +58,18 @@ class EventController extends Controller
     public function show($id)
     {
         $event = Event::findOrFail($id);
-        $event->date = new \DateTime($event->date);
+        $user = auth()->user();
+        $hasUserJoined = false;
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+            foreach ($userEvents as $userEvent) {
+                if ($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -107,5 +116,13 @@ class EventController extends Controller
         $user->eventsAsParticipant()->attach($id);
         $event = Event::findOrFail($id);
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+        return redirect('/dashboard')->with('msg', 'Você sai com sucesso do evento: ' . $event->title);
     }
 }
