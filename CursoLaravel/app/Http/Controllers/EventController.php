761e7b8725626875c4943cbcc481fd3e5b8d3f52
MatheusCervantes <matheuscervantes1@gmail.com>
2024-07-03 14:56:28 -0300

    Aula 29

--- CursoLaravel/app/Http/Controllers/EventController.php
+++ CursoLaravel/app/Http/Controllers/EventController.php
@@ -79,7 +79,11 @@ class EventController extends Controller
 
     public function edit($id)
     {
+        $user = auth()->user();
         $event = Event::findOrFail($id);
+        if ($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
         return view('events.edit', ['event' => $event]);
     }
 
